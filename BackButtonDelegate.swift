3117c3a4dee8d44d234faeb9e8a332a93eb4abab
Gabe Ratcliff <gaberatcliff@gmail.com>
2016-05-17 15:03:07 -0700

    new view controllers, methods

---
+++ BackButtonDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  BackButtonDelegate.swift
+//  Roomie
+//
+//  Created by Gabe Ratcliff on 5/17/16.
+//  Copyright © 2016 Alec Barlow. All rights reserved.
+//
+
+import UIKit
+protocol BackButtonDelegate: class{
+    func backButtonPressedFrom(controller:UITableViewController)
+}
