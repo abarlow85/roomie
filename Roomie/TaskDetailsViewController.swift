6e789f14113815d3d83a65e93b74988c64557f3a
Nigel <nkoh.hs@gmail.com>
2016-05-18 17:23:37 -0700

    created taskdetailsviewcontroller

---
+++ Roomie/TaskDetailsViewController.swift
@@ -0,0 +1,55 @@
+//
+//  TaskDetailsViewController.swift
+//  Roomie
+//
+//  Created by Nigel Koh on 5/18/16.
+//  Copyright © 2016 Alec Barlow. All rights reserved.
+//
+
+import UIKit
+
+class TaskDetailsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var userTableView: UITableView!
+    
+    var taskdetails: NSDictionary?
+    var users: [NSDictionary]?
+    
+    
+    override func viewDidLoad() {
+        userTableView.dataSource = self
+        userTableView.delegate = self
+        let users = taskdetails!["users"] as! NSArray
+        for user in users {
+            let newUser = user as! NSDictionary
+        }
+        
+        super.viewDidLoad()
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return taskdetails!.count
+    }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = userTableView.dequeueReusableCellWithIdentifier("userChoiceCell")!
+        cell.textLabel?.text =  taskdetails![indexPath.row]!["name"] as! String
+        cell.selectionStyle = .None
+        return cell
+    }
+    
+//    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+//        userTableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.Checkmark
+//        let userId = userArray![indexPath.row]["_id"] as! String
+//        responsibleUsers.append(userId)
+//    }
+//    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+//        userTableView.cellForRowAtIndexPath(indexPath)?.accessoryType = UITableViewCellAccessoryType.None
+//        let userId = userArray![indexPath.row]["_id"] as! String
+//        let userIndex = responsibleUsers.indexOf(userId)
+//        responsibleUsers.removeAtIndex(userIndex!)
+//    }
+    
+    
+    
+    
+}
